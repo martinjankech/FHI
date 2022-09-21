9bc200d5e8ab7f915345a25438434a0ee33ed6c4
Martin Jankech <jankech3@gmail.com>
2022-09-21 20:11:44 +0200

    pridane webove sluzby

---
+++ diplomovka/moja_praca/webova_sluzba/elektronicke_knihkupectvo_webove_sluzby_diplomovka/elektronicke_knihkupectvo_webove_sluzby_diplomovka/book_services.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="book_services.asmx.cs" Class="elektronicke_knihkupectvo_webove_sluzby_diplomovka.book_services" %>
---
+++ diplomovka/moja_praca/webova_sluzba/elektronicke_knihkupectvo_webove_sluzby_diplomovka/elektronicke_knihkupectvo_webove_sluzby_diplomovka/book_services.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace elektronicke_knihkupectvo_webove_sluzby_diplomovka
+{
+    /// <summary>
+    /// Summary description for book_services
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class book_services : System.Web.Services.WebService
+    { private String fileBookInfo = "D:\\git_repozitare\\FHI\\diplomovka\\moja_praca\\webova_sluzba\\elektronicke_knihkupectvo_webove_sluzby_diplomovka\\elektronicke_knihkupectvo_webove_sluzby_diplomovka\\book_moje.xml ";
+      private String fileBookTransactionInfo = "D:\\git_repozitare\\FHI\\diplomovka\\moja_praca\\webova_sluzba\\elektronicke_knihkupectvo_webove_sluzby_diplomovka\\elektronicke_knihkupectvo_webove_sluzby_diplomovka\\book_transakcie_moje.xml ";
+
+        [WebMethod]
+        public void SinglebookDataById(int id)
+
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileBookInfo);
+            XmlNodeList AllBook= doc.SelectNodes("Bookstore/books/book");
+            int allBookCount = AllBook.Count;
+            // id su radene od 1... preto tato podmienka je postacujuca
+            if (id <= allBookCount)
+            {
+                XmlNodeList nodeListBook = doc.SelectNodes("Bookstore/books/book[id=" + id + "]");
+                // nastavenie UTF-8 sady pre http response 
+                Context.Response.BinaryWrite(System.Text.Encoding.UTF8.GetPreamble());
+                Context.Response.Write(JsonConvert.SerializeXmlNode(nodeListBook.Item(0), Formatting.Indented));
+            }
+            else
+                Context.Response.Write("nenasiel sa zaznam pre zadane id ");
+
+
+        }
+    }
+}
